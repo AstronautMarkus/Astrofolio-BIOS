5ddebe0c099e774a0125bccb0e06f54ea3739f64
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-04 03:52:41 -0300

    feat: remove GitHub link from Footer component for cleaner presentation

--- astrofolio/src/layout/components/Footer.tsx
+++ astrofolio/src/layout/components/Footer.tsx
@@ -5,7 +5,6 @@ const Footer: React.FC = () => {
     return (
         <footer className="bg-bios-panel p-2 mt-4 text-black text-xs border border-bios-border">
             <div className="flex justify-between">
-            <Link to="https://github.com/AstronautMarkus" target="_blank" rel="noopener noreferrer" className="hover:font-bold">https://github.com/AstronautMarkus/</Link>
             <span>© {new Date().getFullYear()} | AstronautMarkus_Astrofolio V3.0</span>
             </div>
         </footer>
